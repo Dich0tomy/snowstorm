1e7258360081d3482d700d9436060fd127e7dd82
b4mbus <danielzaradny@gmail.com>
2023-05-16 06:49:51 +0200

    ref: remove own highlights to have all that shit in sync

--- modules/home/nvim/nvim/lua/b4mbus/theming/init.lua
+++ modules/home/nvim/nvim/lua/b4mbus/theming/init.lua
@@ -1,9 +1,6 @@
 b4.xpnequire('b4mbus.theming.signs')
-b4.xpnequire('b4mbus.theming.highlights')
 
 b4.xpnequire('b4mbus.theming.statusline')
 b4.xpnequire('b4mbus.theming.tabline')
 b4.xpnequire('b4mbus.theming.winbar')
 b4.xpnequire('b4mbus.theming.statuscolumn')
-
-b4.xpnequire('b4mbus.theming.highlights')
